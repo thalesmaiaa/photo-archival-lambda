1aeb6c29983f5392ce988e4659e7edd71247faf3
Thales Maia <thalesmaiaalexandre@gmail.com>
2025-05-11 21:52:41 -0300

    fix: ensure sendImageMetadata function is asynchronous by awaiting fetch call

--- src/rekognition/sendMetadata.ts
+++ src/rekognition/sendMetadata.ts
@@ -7,9 +7,12 @@ type SendImageMetadata = {
 
 const API_URL = process.env.API_URL;
 
-export function sendImageMetadata({ metadata, objectKey }: SendImageMetadata) {
+export async function sendImageMetadata({
+  metadata,
+  objectKey,
+}: SendImageMetadata) {
   const formattedKey = objectKey.replace(/\//g, '-').replace(/\./g, '-');
-  fetch(`${API_URL}/media/${formattedKey}/metadata`, {
+  await fetch(`${API_URL}/media/${formattedKey}/metadata`, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
